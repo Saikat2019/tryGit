2d9253b4b8ece534b42b0b7663401bceae324bd1
saikat <saikatmondal410@gmail.com>
2018-08-03 15:52:47 +0530

    modified branch saikat 2

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@ int main()
 {
     printf("Hello Git!\n");
     printf("added another line to check git diff\n");
-    printf("checking git diff HEAD");
-    printf("new branch saikat");
+    printf("this will be a diff branch\n");
+    printf("checking git diff HEAD\n");
+    printf("new branch saikat\n");
 
     return 0;
 }
