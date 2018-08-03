4331ac85eb921b3717f9eccf3cb3d8182cd019ab
saikat <saikatmondal410@gmail.com>
2018-08-03 15:54:38 +0530

    modified master 1

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 int main()
 {
     printf("Hello Git!\n");
+    printf("this is the master branch\n");
     printf("added another line to check git diff\n");
     printf("checking git diff HEAD");
+    printf("checking branching\n");
 
     return 0;
 }
